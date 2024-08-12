ef83917de1e291f6b5f8ea0b6560aeec49e63405
satyajitPatel <thriver1988@gmail.com>
2024-08-12 11:26:51 +0530

    add hooks and icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
-import Box from '../components/box/App.jsx'
-import App from '../components/hello/App.jsx'
-import Nav from '../components/navBar/App.jsx'
+import Box from './components/box/App.jsx'
+import App from './components/hello/App.jsx'
+import Nav from './components/navBar/App.jsx'
 
 
 
@@ -11,6 +11,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
     <App msg1='welcome folks' msg2='how you guys doing' />
-    <Box />
+    <Box description="An all-rounder is a cricketer who regularly performs well at both batting and bowling. Although all bowlers must bat and quite a handful of batsmen do bowl occasionally, most players are skilled in only one of the two disciplines and are considered specialists." />
   </StrictMode>,
 )
