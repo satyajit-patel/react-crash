c97b9efa77e17028158337beecf50d7a4a04b053
satyajitPatel <thriver1988@gmail.com>
2024-09-15 23:16:38 +0530

    useContext Hook - no more chaining props

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,44 +1,49 @@
-import {TextCard} from './components/aceternityUI/textReveal/TextCard';
+import { createContext, useState } from 'react';
+import NewApp from './NewApp';
+
+// create the context
+// wrap with provider
+// use the context
+const ImranKhan = createContext();
 
 function App() {
-    const data = [
-        {
-          quote: "The only constant is change.",
-          author: "Heraclitus"
-        },
-        {
-          quote: "I know that I know nothing.",
-          author: "Socrates"
-        },
-        {
-          quote: "Less is more.",
-          author: "Ludwig Mies van der Rohe"
-        },
-        {
-          quote: "To lead the people, walk behind them.",
-          author: "Lao Tzu"
-        },
-        {
-          quote: "The more you learn, the less you know.",
-          author: "Socrates"
-        },
-        {
-          quote: "The pursuit of happiness is the source of unhappiness.",
-          author: "Blaise Pascal"
-        }
-    ];
+  const data = [
+    {
+      quote: "The only constant is change.",
+      author: "Heraclitus"
+    },
+    {
+      quote: "I know that I know nothing.",
+      author: "Socrates"
+    },
+    {
+      quote: "Less is more.",
+      author: "Ludwig Mies van der Rohe"
+    },
+    {
+      quote: "To lead the people, walk behind them.",
+      author: "Lao Tzu"
+    },
+    {
+      quote: "The more you learn, the less you know.",
+      author: "Socrates"
+    },
+    {
+      quote: "The pursuit of happiness is the source of unhappiness.",
+      author: "Blaise Pascal"
+    }
+  ];
 
-    return (
-      <div className='flex flex-row flex-wrap justify-evenly bg-slate-800'>
-        {/* <div><TextCard/></div>
-        <div><TextCard/></div> */}
-        {
-          data.map((it) => (
-            <div key={it.id}><TextCard it={it} /></div>
-          ))
-        }
-      </div>
-    );
+  const [x, setX] = useState({nums: data});
+
+  return (
+    <div>
+      <ImranKhan.Provider value={{x, setX}}>
+        <NewApp />
+      </ImranKhan.Provider>
+    </div>
+  );
 }
 
 export default App;
+export {ImranKhan};
