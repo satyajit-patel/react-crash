f052d8685be1281e8cf95fe27455435fa57da98b
satyajitPatel <thriver1988@gmail.com>
2024-09-15 21:18:16 +0530

    There is Hook in me. One that you can not see. But it is there. You think I have a choice. There is no fucking choice

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,30 +1,100 @@
-import React, { useState, useEffect } from 'react';
-import FlashcardList from './components/FlashcardList';
-import AdminDashboard from './components/AdminDashboard';
-import { fetchFlashcards } from './apis/Api';
-import Card from "./components/Card";
-import './styles.css';
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
-    const [flashcards, setFlashcards] = useState([]);
-
-    useEffect(() => {
-        async function getFlashcards() {
-            const data = await fetchFlashcards();
-            setFlashcards(data);
+    const data = [
+        {
+          quote: "The only constant is change.",
+          author: "Heraclitus"
+        },
+        {
+          quote: "I know that I know nothing.",
+          author: "Socrates"
+        },
+        {
+          quote: "Less is more.",
+          author: "Ludwig Mies van der Rohe"
+        },
+        {
+          quote: "To lead the people, walk behind them.",
+          author: "Lao Tzu"
+        },
+        {
+          quote: "The more you learn, the less you know.",
+          author: "Socrates"
+        },
+        {
+          quote: "The pursuit of happiness is the source of unhappiness.",
+          author: "Blaise Pascal"
         }
-        getFlashcards();
-    }, []);
+    ];
+      
+
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [B2Text, setB2Text] = useState("B-2");
+    const [loading, setLoading] = useState(true);
+    const [nums, setNums] = useState([]);
+
+    const handleB1 = () => {
+        setCount1(count1 + 1);
+    }
+    const handleB2 = () => {
+      setB2Text("clicked");
+      setCount2(count2 + 1);
+      setTimeout(() => {
+        setB2Text("B-2");
+      }, 1000);
+    }
+
+    // // with empty dependencies
+    // useEffect(() => {
+    //     alert("I will run once at very first render");
+    // }, []);
+
+    const dp = useCallback(() => {
+      const getData = () => {
+        setNums(data);
+        setLoading(false);
+      }
+      getData();
+    }, [])
+
+    // with dependencies
+    useEffect(() => {
+        // alert("I will run every time when rerender/count1/count2/setNums gets updated");
+        dp();
+    }, [count1, count2, setNums])
 
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Navigate to="/FlashcardList" />} />
-                <Route path='/FlashcardList' element={<FlashcardList arr={flashcards} />} />
-                <Route path='/AdminDashboard' element={<AdminDashboard setFlashcards={setFlashcards} />} />
-            </Routes>
-        </Router>
+        <div className="h-screen w-screen">
+            <div className="bg-slate-500 flex flex-wrap justify-center items-center">
+                <div className="bg-slate-300 rounded-md h-[100px] w-[150px] text-center m-2 p-2">
+                    <button onClick={handleB1} className="bg-black text-white h-12 w-14 rounded-md">B-1</button>
+                    <p>Count is: {count1}</p>
+                </div>
+                <div className="bg-slate-300 rounded-md h-[100px] w-[150px] text-center m-2 p-2">
+                    <button onClick={handleB2} className="bg-black text-white h-12 w-14 rounded-md">{B2Text}</button>
+                    <p>Count is: {count2}</p>
+                </div>
+            </div>
+            <div className="bg-black">
+                {
+                    loading ? 
+                    (
+                        <h1 className="text-center text-white">Loading Please Wait.....</h1>
+                    ): 
+                    (
+                        nums.map((it) => (
+                            <div key={it.id} className="text-white text-center">
+                                <p>{it.quote}</p>
+                                <p>~{it.author}</p>
+                                <br />
+                            </div>
+                        ))
+                    )
+                }
+            </div>
+        </div>
     );
 }
 
